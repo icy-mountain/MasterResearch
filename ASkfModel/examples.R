ac57f673acf569ef82929732a1f8bdc2ddeacff7
icy-mountain <ym.1211@icloud.com>
2023-02-27 08:55:09 +0000

    fix: fix comment, add output=FALSE case

--- ASkfModel/examples.R
+++ ASkfModel/examples.R
@@ -9,7 +9,7 @@ source("./ASkfModel.R")
 # params$f <- "(1 - t)^2" # χ2-divergence (Pearsonian distance).
 # params$f <- "-log(t)" # reverse KL-divergence.
 # params$f <- "t * log(t)" # KL-divergence.
-# params$f <- "(3*(3 + 1))^(-1) * (t^(4) - t)" # power-divergence lambda = 3
+# params$f <- "(t^(4) - t) / 12" # power-divergence lambda = 3
 
 freq <-
   c(98,   150,  135,   53,
@@ -18,7 +18,9 @@ freq <-
     4,    1,    4,    21)
 # freq <- tab1_freq
 ASkf_result <- DisplayASkfResult(freq=freq, f="t * log(t)", name="t", score=1:4, k=3)
+ASkf_result <- DisplayASkfResult(freq=freq, f="t * log(t)", name="t", score=1:4, k=3, output=FALSE)
 ASkf_result <- DisplayASkfResult(freq=freq, f="(1 - t)^2", name="t", score=1:4, k=3)
+ASkf_result <- DisplayASkfResult(freq=freq, f="(t^(4) - t) / 12", name="t", score=1:4, k=3)
 ASkf_result <- DisplayASkfResult(freq=freq, f="-log(t)", name="t", score=1:4, k=3)
 ASkf_result <- DisplayASkfResult(freq=freq, f="(1 - sqrt(x))^2", name="x", score=1:4, k=3)
 ASkf_result <- DisplayASkfResult(freq=freq, f="-(x+1) * log((x+1)/2) + x*log(x)", name="x", score=1:4, k=3)
